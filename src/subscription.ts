52e696d51e16c935fde655cfa96b64576f448595
Chris Chabot <chabotc@Chriss-MacBook-Pro.local>
2023-05-18 16:21:29 +0100

    Write firehose events to memgraph. ops.likes.deletes and ops.follows.deletes are still TODO

--- src/subscription.ts
+++ src/subscription.ts
@@ -3,181 +3,123 @@ import {
   isCommit,
 } from './lexicon/types/com/atproto/sync/subscribeRepos'
 import { FirehoseSubscriptionBase, getOpsByType } from './util/subscription'
-var neo4j = require('neo4j-driver')
+const neo4j = require('neo4j-driver')
+const util = require('util')
+const driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("", ""), { encrypted: 'ENCRYPTION_OFF' })
+const session = driver.session()
+
+async () => {
+  await session.run("CREATE INDEX ON :Person(did)", {})
+  await session.run("CREATE INDEX ON :Post(uri)", {})
+}
 
 export class FirehoseSubscription extends FirehoseSubscriptionBase {
   async handleEvent(evt: RepoEvent) {
     if (!isCommit(evt)) return
     const ops = await getOpsByType(evt)
-    const driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("", ""), { encrypted: 'ENCRYPTION_OFF' })
-    const session = driver.session()
-
-/*
-[follow delete]
-{
-  uri: 'at://did:plc:oftvwqwimefeuzes4nwinubh/app.bsky.graph.follow/3jvcsau7em22q'
-}
-
-[follow create]
-{
-  record: {
-    '$type': 'app.bsky.graph.follow',
-    subject: 'did:plc:nkz2qw7c47td7lw542jtn6tu',
-    createdAt: '2023-05-18T10:53:47.530Z'
-  },
-  uri: 'at://did:plc:4bes3fnl6ngecjyt3ky43236/app.bsky.graph.follow/3jvyqjbxok32l',
-  cid: 'bafyreihmq25edax2jsq7b7dmzwk54juhnbaroswvczt3bspcsfq7fedyuq',
-  author: 'did:plc:4bes3fnl6ngecjyt3ky43236'
-}
-
-[post delete]
-at://did:plc:37s5sfi4ln4hzn5rgc7egboy/app.bsky.feed.post/3jvxpjb3mc32g
-
-[post create]
-{
-  record: {
-    text: 'Obrigado lindo 🤓',
-    '$type': 'app.bsky.feed.post',
-    reply: { root: [Object], parent: [Object] },
-    createdAt: '2023-05-18T10:51:30.874Z'
-  },
-  uri: 'at://did:plc:exphjormp2sojr26gagp6v4f/app.bsky.feed.post/3jvyqf7gvct2g',
-  cid: 'bafyreieiptaezvbowxjujujhqqf5kpp57oufumf65vwsa7x7zyls3gktxm',
-  author: 'did:plc:exphjormp2sojr26gagp6v4f'
-}
-
-[like delete]
-{
-  uri: 'at://did:plc:qxwxmbcgzs3jxo7xsmh5nlwj/app.bsky.feed.like/3jvyqkg4k362f'
-}
-
-[like create]
-{
-  record: {
-    '$type': 'app.bsky.feed.like',
-    subject: {
-      cid: 'bafyreia7r2nn7wbeyfaiowu3x3zbiundjn3tlgevvkvmhol7bwdnfkr4yi',
-      uri: 'at://did:plc:llccg573ujhlnt7ulk3wiyzt/app.bsky.feed.post/3jvyqeszkhs2l'
-    },
-    createdAt: '2023-05-18T10:51:30.260Z'
-  },
-  uri: 'at://did:plc:s4vvu2hmagegff4z3wpy4xxp/app.bsky.feed.like/3jvyqf6w7532g',
-  cid: 'bafyreiakm7nkrmgdio7loxj7nkcrayso3dpjtsxkdkjqw7omna2hpdhw54',
-  author: 'did:plc:s4vvu2hmagegff4z3wpy4xxp'
-}
-
-[repost delete]
-{
-  uri: 'at://did:plc:xydmw4rhlnb4mrz5eyw3z6ak/app.bsky.feed.repost/3jvyqkptvwt2g'
-}
-
-[repost create]
-{
-  record: {
-    '$type': 'app.bsky.feed.repost',
-    subject: {
-      cid: 'bafyreiapagaipbf4sflechnws62n4reyj3hikczt6pu5cj6w3oi5bywpwe',
-      uri: 'at://did:plc:zplbw4hvwrkp6fqopwe2inod/app.bsky.feed.post/3jvyqanedew2f'
-    },
-    createdAt: '2023-05-18T10:52:39.573Z'
-  },
-  uri: 'at://did:plc:izm3fauw3hx4pspgzc5i76ht/app.bsky.feed.repost/3jvyqhayjdk2l',
-  cid: 'bafyreibf3mbzpokmjbpcrjnjakzjvivbw4l45yph5lqgszwhghqltatrzu',
-  author: 'did:plc:izm3fauw3hx4pspgzc5i76ht'
-}
-
-
-
-*/
-
-
 
     if (ops.posts.deletes.length > 0) {
       for (const post of ops.posts.deletes) {
-        console.log('[post delete]')
-        console.log(post.uri)
+        process.stdout.write('P')
+        //process.stdout.write(util.inspect(post, false, null, true))
+        try {
+          await session.run("MATCH (p:Post {uri: $uri}) DETACH DELETE p", { uri: post.uri })
+        } catch (err) {
+          console.error('[ERROR POST DELETE]:', err)
+        }
       }
     }
 
     if (ops.posts.creates.length > 0) {
       for (const post of ops.posts.creates) {
-        console.log('[post create]')
-        console.log(post)
+        process.stdout.write('p')
+        //process.stdout.write(util.inspect(post, false, null, true))
+        try {
+          await session.run("CREATE (p:Post {uri: $uri, cid: $cid, author: $author, text: $text, createdAt: $createdAt}) RETURN p", { uri: post.uri, cid: post.cid, author: post.author, text: post.record.text, createdAt: post.record.createdAt })
+          await session.run("CREATE a = (:Person {did: $did})-[:AUTHOR_OF]->(:Post {uri: $postUri}) RETURN a", { postUri: post.uri, did: post.author})
+          await session.run("CREATE a = (:Post {uri: $postUri})-[:AUTHOR]->(:Person {did: $did}) RETURN a", { postUri: post.uri, did: post.author })
+          
+          const replyRoot = post.record?.reply?.root ? post.record.reply.root.uri : null
+          const replyParent = post.record?.reply?.parent ? post.record.reply.parent.uri : null
+          if (replyRoot) {
+            await session.run("CREATE r = (:Post {uri: $uri})-[:ROOT]->(:Post {uri: $rootUri}) RETURN r", { uri: post.uri, rootUri: replyRoot })
+          }
+          if (replyParent) {
+            await session.run("CREATE r = (:Post {uri: $uri})-[:PARENT]->(:Post {uri: $parentUri}) RETURN r", { uri: post.uri, parentUri: replyParent })
+          }
+        } catch (err) {
+          console.error('[ERROR POST CREATE]:', err)
+        }
       }
     }
 
     if (ops.follows.deletes.length > 0) {
       for (const follow of ops.follows.deletes) {
-        console.log('[follow delete]')
-        console.log(follow)
+        process.stdout.write('F')
+        //FIXME the record only contains a URI without a source -> dest DID mapping. There is a URI in the at:// uri, but no destination is specificed
+        // So not sure yet how to delete the follow relationship in a graph 
+        //{
+        //  uri: 'at://did:plc:oftvwqwimefeuzes4nwinubh/app.bsky.graph.follow/3jvcsau7em22q'
+        //}
+        //process.stdout.write(util.inspect(follow, false, null, true))
       }
     }
 
     if (ops.follows.creates.length > 0) {
       for (const follow of ops.follows.creates) {
-        console.log('[follow create]')
-        console.log(follow)
+        process.stdout.write('f')
+        //process.stdout.write(util.inspect(follow, false, null, true))
+        try {
+          await session.run("CREATE f = (:Person {did: $authorDid})-[:FOLLOW]->(:Person {did: $subjectDid}) RETURN f", { authorDid: follow.author, subjectDid: follow.record.subject })
+        } catch (err) {
+          console.error('[ERROR POST CREATE]:', err)
+        }
       }
     }
 
     if (ops.likes.deletes.length > 0) {
       for (const like of ops.likes.deletes) {
-        console.log('[like delete]')
-        console.log(like)
+        process.stdout.write('L')
+        //FIXME sane situation as with follows.delete, just a URI and not a full source -> dest mapping
+        //process.stdout.write(util.inspect(like, false, null, true))
       }
     }
 
     if (ops.likes.creates.length > 0) {
       for (const like of ops.likes.creates) {
-        console.log('[like create]')
-        console.log(like)
+        process.stdout.write('l')
+        //process.stdout.write(util.inspect(like, false, null, true))
+        try {
+          await session.run("CREATE l = (:Person {did: $authorDid})-[:LIKE]->(:Post {uri: $postUri}) RETURN l", { authorDid: like.author, postUri: like.record.subject.uri })
+        } catch (err) {
+          console.error('[ERROR POST CREATE]:', err)
+        }
+
       }
     }
 
     if (ops.reposts.deletes.length > 0) {
       for (const repost of ops.reposts.deletes) {
-        console.log('[repost delete]')
-        console.log(repost)
+        process.stdout.write('R')
+        //process.stdout.write(util.inspect(repost, false, null, true))
+        try {
+          await session.run("MATCH (p:Post {uri: $uri}) DETACH DELETE p", { uri: repost.uri })
+        } catch (err) {
+          console.error('[ERROR REPOST DELETE]:', err)
+        }
       }
     }
 
     if (ops.reposts.creates.length > 0) {
       for (const repost of ops.reposts.creates) {
-        console.log('[repost create]')
-        console.log(repost)
+        process.stdout.write('r')
+        //process.stdout.write(util.inspect(repost, false, null, true))
+        try {
+          await session.run("CREATE (p:Post {uri: $uri, cid: $cid, author: $author, repostUri: $repostUri, createdAt: $createdAt}) RETURN p", { uri: repost.uri, cid: repost.cid, author: repost.author, repostUri: repost.record.subject.uri, createdAt: repost.record.createdAt })
+        } catch (err) {
+          console.error('[ERROR REPOST CREATE]:', err)
+        }
       }
     }
-    /*
-    const postsToDelete = ops.posts.deletes.map((del) => del.uri)
-    const postsToCreate = ops.posts.creates
-      .filter((create) => {
-        // only alf-related posts
-        return create.record.text.toLowerCase().includes('alf')
-      })
-      .map((create) => {
-        // map alf-related posts to a db row
-        return {
-          uri: create.uri,
-          cid: create.cid,
-          replyParent: create.record?.reply?.parent.uri ?? null,
-          replyRoot: create.record?.reply?.root.uri ?? null,
-          indexedAt: new Date().toISOString(),
-        }
-      })
-
-    if (postsToDelete.length > 0) {
-      await this.db
-        .deleteFrom('post')
-        .where('uri', 'in', postsToDelete)
-        .execute()
-    }
-    if (postsToCreate.length > 0) {
-      await this.db
-        .insertInto('post')
-        .values(postsToCreate)
-        .onConflict((oc) => oc.doNothing())
-        .execute()
-    }
-    */
   }
 }
