90ba32180683bc8f1014a61879231206fb78dffc
katarinasupe <supe.katarina@gmail.com>
2023-05-22 13:41:15 +0200

    Fix typo

--- src/subscription.ts
+++ src/subscription.ts
@@ -11,7 +11,7 @@ const session = driver.session()
 async () => {
   await session.run("CREATE INDEX ON :Person(did)", {})
   await session.run("CREATE INDEX ON :Post(uri)", {})
-  await session.run("CREATE INDEX ON: Person", {})
+  await session.run("CREATE INDEX ON :Person", {})
   await session.run("CREATE INDEX ON :Post", {})
 }
 
