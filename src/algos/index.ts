7d4bb18a1e9aa4700a0fbe8996a3e6e5f5bfc506
Chris Chabot <chabotc@gmail.com>
2023-06-08 21:54:46 +0000

    Rebranded friends and community to home plus so it can be published on the memgraph.com account on bluesky

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -4,11 +4,13 @@ import {
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
 import * as FriendsAndCommunity from './friends-and-community'
+import * as HomePlus from './home-plus'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams, requesterDid: string) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
   [FriendsAndCommunity.uri]: FriendsAndCommunity.handler,
+  [HomePlus.uri]: HomePlus.handler,
 }
 
 export default algos
