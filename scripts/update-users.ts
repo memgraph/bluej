4157f1b559f918e9f0dec1f3bd38f9f2f57f62db
mBagaric <marko.bagarich@hotmail.com>
2023-08-08 12:17:56 +0200

    Finishing update users script

--- scripts/update-users.ts
+++ scripts/update-users.ts
@@ -11,6 +11,7 @@ const agent = new BskyAgent({
 })
 
 let iterationTimes: number[] = [];
+let updatedUserDidList: string[] = [];
 let errorList: any[] = [];
 
 //Parent function of updating users
@@ -20,22 +21,24 @@ async function users(){
     
     try {
         const session = driver.session()
-        let list = await session.run(" MATCH (n:Person) WHERE n.handle IS NULL RETURN n; "); //Fetching all users that don't have handle (not updated ones)
+        let list = await session.run(" MATCH (n:Person) WHERE n.handle IS NULL RETURN n LIMIT 3000; "); //Fetching all users that don't have handle (not updated ones)
         Array.prototype.push.apply(userList, list.records);
         session.close()
     } catch (err) {
         errorList.push(["saveUsers", err]);
-        console.error('[saveUsers]:', err)
+        console.error('[saveUsers]:', err);
     }
 
     //Recursive function of updating all fetched users
     iterationTimes = [];
-    if(userList === undefined || userList === null){
+    if(userList === undefined || userList === null || userList.length === 0){
         console.log("Couldn't load user list. Please try again!")
-        return;
+        return new Map<string, number>([
+            ["error", 1]
+        ])
     }
     else{
-        
+        console.log("Running update user script...");
         let updatedUserList = await updateUsers(userList, firstCalled, 0, 0, 0);
         if(updatedUserList != undefined && updatedUserList != null){
             userList = updatedUserList;
@@ -50,84 +53,70 @@ async function users(){
 
 async function updateUsers(userList: any[], firstCalled: number,
         numberOfUpdatedUsers: number, iterationNumber: number, waitingTime: number){
-
-    let endOfIteration = false
-
-    while(!endOfIteration){
-        if(userList.length === 0){
-            endOfIteration = true;
-            break;
-        }
-
-        let startIteration = Date.now(); //Time of updating 5 users
-        let numOfRepeat = (userList.length > 5) ? 5 : userList.length; //Finding remaining number of users in the list
-        let lookingList : any[] = []; //List containing only dids for easier use
-
-        console.log("Remaining users to update: " + userList.length)
-
-        for(let i=0; i < numOfRepeat; i++){
-            const node : Map<string, any> | undefined = userList.pop();
-            if(node){
-                lookingList.push(node["_fields"][0]["properties"]["did"]);
-            }
-        }
-        let saveValues: any[] = []; //List containing all necessary info for updating users
-
-        console.log("Updating users with a did: ")
-        console.log(lookingList);
-
-        let numberOfSuccess = 0;
-        const session = driver.session();
-        const promises = lookingList.map(async (did) => {
-            let newItem = await getInfo(did);
-            if(newItem != undefined && newItem != null){
-            let saveValues = await saveOneToDb(newItem, session);
-                if(saveValues){
-                    numberOfUpdatedUsers += 1;
-                    numberOfSuccess += 1;
-                }
-            }
-        });
-
-        await Promise.all(promises);
-        session.close();
-
-        console.log(`Saved ${numberOfSuccess} users`);
-
-        let endIteration = Date.now(); //Time of end for updating 5 users
-        let durationOfIteration = endIteration.valueOf() - startIteration.valueOf() //How long did update of 5 users take
-        let durationOfRun = endIteration.valueOf() - firstCalled.valueOf(); //How long is updating whole list taking
-        console.log(`Duration of 1 iteration: ` + durationOfIteration);
-        console.log(`Number of saved users: ` + numberOfUpdatedUsers);
-
-        iterationTimes.push(durationOfIteration);
-
-        //Return the rest if 5min had passed
-        if((Date.now().valueOf() - firstCalled) > (5 * 60 * 1000)){
-            printInfoForTheEnd(userList.length, numberOfUpdatedUsers, durationOfRun, durationOfIteration);
-            console.log("------------------------------------------") 
-            console.log("Ended 5 minute interval")
-            console.log("------------------------------------------");
-            endOfIteration = true;
-            break;
-        }
-
-        //Calculating waiting time before next iteration so it evenly spreads over 5 minutes
-        if(iterationNumber % 10 === 0){
-            printInfoForTheEnd(userList.length, numberOfUpdatedUsers, durationOfRun, durationOfIteration);
-            waitingTime = calculateWaitingTime(numberOfUpdatedUsers, durationOfRun)
-        }
-
-        console.log("Resting...")
-        await delay(waitingTime);
-        iterationNumber += 1;
+    if(userList.length === 0){
+        return [];;
     }
-    return userList;
+
+    let startIteration = Date.now(); //Time of updating 5 users
+    let numOfRepeat = (userList.length > 5) ? 5 : userList.length; //Finding remaining number of users in the list
+    let lookingList : any[] = []; //List containing only dids for easier use
+
+    for(let i=0; i < numOfRepeat; i++){
+        const node : Map<string, any> | undefined = userList.pop();
+        if(node){
+            lookingList.push(node["_fields"][0]["properties"]["did"]);
+        }
+    }
+    let saveValues: any[] = []; //List containing all necessary info for updating users
+
+    let numberOfSuccess = 0;
+    const session = driver.session();
+    const promises = lookingList.map(async (did) => {
+        let newItem = await getInfo(did);
+        if(newItem != undefined && newItem != null){
+            let saveValues = await saveOneToDb(newItem, session);
+            if(saveValues){
+                numberOfUpdatedUsers += 1;
+                numberOfSuccess += 1;
+                updatedUserDidList.push(did);
+            }
+        }
+    });
+
+    await Promise.all(promises);
+    session.close();
+
+    let endIteration = Date.now(); //Time of end for updating 5 users
+    let durationOfIteration = endIteration.valueOf() - startIteration.valueOf() //How long did update of 5 users take
+    let durationOfRun = endIteration.valueOf() - firstCalled.valueOf(); //How long is updating whole list taking
+
+    iterationTimes.push(durationOfIteration);
+
+    //Return the rest if 5min had passed
+    if((Date.now().valueOf() - firstCalled) > (5 * 60 * 1000)){
+        let sum = 0;
+        iterationTimes.forEach(async (time) => {
+            sum += time;
+        })
+        let average = Math.round(sum/iterationTimes.length)
+        console.log("Updated users: ");
+        console.log(updatedUserDidList)
+        console.log(`Number of updated users: ${numberOfUpdatedUsers}; Average iteration time: ${average}`)
+        return userList;
+    }
+
+    //Calculating waiting time before next iteration so it evenly spreads over 5 minutes
+    if(iterationNumber % 10 === 0){;
+        waitingTime = calculateWaitingTime(numberOfUpdatedUsers, durationOfRun)
+    }
+
+    await delay(waitingTime);
+    return await updateUsers(userList, firstCalled, numberOfUpdatedUsers, iterationNumber += 1, waitingTime)
 }
 
 //Calling api for getting info about users and returning its value
 async function getInfo(did: string){
-    let user = await fetchProfile(did)
+    let user = await fetchProfile(did);
     if(user != undefined && user != null){
         let wantedValues = new Map<string, any>([
             ["did", user?.data.did],
@@ -139,7 +128,7 @@ async function getInfo(did: string){
             ["displayName", user?.data.displayName]]);
         return wantedValues;
     }
-    return {};
+    return undefined;
 }
 
 //Saving one value to db
@@ -174,16 +163,6 @@ async function saveOneToDb(item: any, session: any){
         console.error('[saveToDb]:', err);
     }
     return state;
-}
-
-//Only for checking purposes prints some values
-function printInfoForTheEnd (remainingUsersToUpdate: number, numberOfUpdatedUsers: number, durationOfRun: number, durationOfIteration: number){
-    console.log("------------------------------------------") 
-    console.log("Remaining users to update: " + remainingUsersToUpdate);
-    console.log("Number of updated users: " + numberOfUpdatedUsers);
-    console.log("Duration of run: " + durationOfRun);
-    console.log("Duration of iteration: " + durationOfIteration);
-    console.log("------------------------------------------");
 }
 
 //Calculates new waiting time every 10 iterations based on average iteration time
@@ -221,17 +200,23 @@ function delay(ms: number): Promise<void> {
 //Running login and startting parent function of updating users
 async function run(){
     await agent.login({ identifier: <string>process.env.FEEDGEN_HANDLE, password: <string>process.env.FEEDGEN_PASSWORD })
+    let gettingUserErrorCount = 0;
     while(true){
         let returnValue = await users();
         if(returnValue != undefined){
-            console.log("Found errors: ");
-            console.log(errorList);
-            if(returnValue.get("duration") < (5*60*1000)){
+            if(returnValue.get("error") === 1){
+                gettingUserErrorCount += 1;
+            }
+            else if(returnValue.get("duration") < (5*60*1000)){
+                gettingUserErrorCount = 0;
+                console.log("Finished current list of users for update")
                 console.log("Resting for " + (((5*60*1000) - returnValue.get("duration"))/1000) + " seconds...")
                 await delay((5*60*1000) - returnValue.get("duration"))
             }
-            console.log(returnValue)
-            console.log("Resting for 5 seconds...");
+            if(gettingUserErrorCount >= 5){
+                console.log("Remaining user list is empty or users can not be fetched.");
+                break;
+            }
             await delay(5000);
         }
         else{
@@ -239,6 +224,9 @@ async function run(){
             break;
         }
     }
+    console.log("Found errors: ");
+    console.log(errorList);
+    process.exit(1)
 }
 
 run()
